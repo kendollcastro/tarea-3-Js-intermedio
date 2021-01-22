27c81a1924644928f1e3ef59307fb6d43d0053bc
Kendoll Castro M <kendollcastrom.@MacBook-Pro-de-Kendoll.local>
2021-01-21 18:34:20 -0600

    agregamos todos los archivos al repositorio

---
+++ script.js
@@ -0,0 +1,52 @@
+/**
+ * Ejercicio #3:
+ * Usando querySelector y querySelectorAll
+ *
+ * ¿Que voy a evaluar?
+ * 1. Que funcione
+ * 2. Buenas practicas
+ *
+ * NOTA: no se puede modificar el codigo HTML de la pagina
+ */
+
+/*
+  EJERCICIO A: --------------------------------------------
+  
+  Selecciona y console.log el <h3> que dice "Esto
+  es una lista"
+*/
+
+const heading = document.querySelector('h3');
+console.log(heading);
+
+/*
+  EJERCICIO B: --------------------------------------------
+  
+  Selecciona y loguea el primer y el ultimo elemento
+  de la lista
+*/
+const lista = document.querySelectorAll('li');
+console.log(lista[0]);
+console.log(lista[3]);
+/*
+  EJERCICIO C: --------------------------------------------
+  
+  Selecciona y loguea el input de tipo "email" del
+  formulario
+*/
+
+const element = document.querySelectorAll('input');
+console.log(element[2]);
+
+/*
+  EJERCICIO D: --------------------------------------------
+  
+  Selecciona y loguea la fila de la tabla que tiene "JavaScript"
+*/
+const tabla = document.querySelectorAll('tr');
+console.log(tabla[2]);
+/*
+  EJERCICIO E: --------------------------------------------
+  
+  Selecciona y loguea la celda que dice CSS
+*/
